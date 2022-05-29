d5120f904de8f7d3662363865c2a2da3d6ada45f
翁喆 <zhe_weng@163.com>
2022-05-30 01:15:20 +0800

    尝试修改初始化方式，但似乎不太有用

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 __author__ = 'WZ'
 
-import asyncio
+import time
 
 from blivedm.blivedm import BLiveClient
 
@@ -10,29 +10,21 @@ from common import config, db
 from handlers import GiftStatsHandler
 
 
-async def listen_rooms():
-    clients = [BLiveClient(room_id) for room_id in config.room_id()]
-    handler = GiftStatsHandler(db)
-    for client in clients:
-        client.add_handler(handler)
-        client.start()
-
-    try:
-        await asyncio.gather(*(
-            client.join() for client in clients
-        ))
-    finally:
-        await asyncio.gather(*(
-            client.stop_and_close() for client in clients
-        ))
+class LiveClientManager:
+    def __init__(self):
+        self.clients = [BLiveClient(room_id) for room_id in config.room_id()]
+        self.handler = GiftStatsHandler(db)
+        for client in self.clients:
+            client.add_handler(self.handler)
+            client.start()
 
 
-async def main():
+def main():
     config.load()
     db.init(config.database_url())
-    live_future = asyncio.ensure_future(listen_rooms())
-    await asyncio.shield(live_future)
+    manager = LiveClientManager()
+    time.sleep(10000)
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
