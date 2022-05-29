7e2b948eda95517dbae7e10f101372802648c771
翁喆 <zhe_weng@163.com>
2022-05-30 01:32:45 +0800

    增加初版web_server

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 __author__ = 'WZ'
 
 import asyncio
+import threading
 
 from blivedm.blivedm import BLiveClient
 
 from common import config, db
 from handlers import GiftStatsHandler
+from web_server import app as application
 
 
 async def listen_rooms():
@@ -27,12 +29,13 @@ async def listen_rooms():
         ))
 
 
-async def main():
+def main():
     config.load()
     db.init(config.database_url())
-    live_future = asyncio.ensure_future(listen_rooms())
-    await asyncio.shield(live_future)
+    # 如果使用debug模式，需要修改use_reloader为False，因为use_reloader预期自己在主线程
+    threading.Thread(target=application.run, daemon=True, kwargs={'host': '::'}).start()
+    asyncio.get_event_loop().run_until_complete(listen_rooms())
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
---
+++ web_server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+__author__ = 'WZ'
+
+from flask import Flask, Response
+
+from common import db
+from tables import GiftType, GiftStatsTable
+
+app = Flask(__name__)
+
+
+def empty_response():
+    return Response(status=200)
+
+
+@app.route('/')
+def root():
+    return empty_response()
+
+
+@app.route('/<room_id>/<gtype>')
+def show_all(room_id: int, gtype: GiftType):
+    result = f"房间{room_id} 礼物类型{gtype}\n"
+    with db.session() as session:
+        lines = session.query(GiftStatsTable).filter_by(type=gtype, rid=room_id)
+    return result + '\n'.join(f"{line.__dict__}" for line in lines)
