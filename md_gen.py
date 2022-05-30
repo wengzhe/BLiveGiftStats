fcf51bc0e0eb54c1fd6ea934e92fa9139e249b08
翁喆 <zhe_weng@163.com>
2022-05-30 21:57:35 +0800

    split out md_gen.py

---
+++ md_gen.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+__author__ = 'WZ'
+
+import sqlalchemy
+
+from common import db, Utils
+from tables import GiftType, GiftStatsTable
+
+
+class GiftStatsLine:
+    def __init__(self, key, name, obj, func):
+        self.key = key
+        self.name = name
+        self.obj = obj
+        self.func = func
+
+
+gift_stats_line_list = [
+    GiftStatsLine('uid', 'uid', GiftStatsTable.uid, lambda i: i),
+    GiftStatsLine('uname', '用户名', GiftStatsTable.uname, lambda i: i),
+    GiftStatsLine('gid', '礼物id', GiftStatsTable.gid, lambda i: i),
+    GiftStatsLine('gname', '礼物名', GiftStatsTable.gname, lambda i: i),
+    GiftStatsLine('num', '数量', sqlalchemy.func.sum(GiftStatsTable.num), lambda i: i),
+    GiftStatsLine('price', '单价（元）', sqlalchemy.func.avg(GiftStatsTable.price), lambda i: i / 1000),
+    GiftStatsLine('total', '总价（元）', sqlalchemy.func.sum(GiftStatsTable.total), lambda i: float(i) / 1000),
+    GiftStatsLine('time', '时间', sqlalchemy.func.max(GiftStatsTable.time), lambda i: Utils.time_from_unix(i)),
+]
+
+
+def get_md_table_line(items):
+    return '|' + '|'.join(str(i) for i in items) + '|\n'
+
+
+def get_md_from_line(line):
+    return get_md_table_line(v.func(line[i]) for i, v in enumerate(gift_stats_line_list))
+
+
+def get_order_obj(order_by: int):
+    key = getattr(GiftStatsTable, gift_stats_line_list[abs(order_by) - 1].key)
+    return key.asc() if order_by > 0 else key.desc()
+
+
+def get_query(url_args: dict, **kwargs):
+    return "&".join(f"{k}={v}" for k, v in {**url_args, **kwargs}.items())
+
+
+def get_md_from_room_gift(room_id: int, gtype: GiftType, url_args: dict):
+    order_by = int(url_args.get('order', -len(gift_stats_line_list)))
+    aggregate = url_args.get('aggregate', 'false').lower() == 'true'
+    md = f"# 房间 {room_id} 的 {gtype.to_string()} 情况\n"
+    md += f"[聚合](?{get_query(url_args, aggregate=not aggregate)})\n"
+    md += get_md_table_line(
+        f'[{v.name}](?{get_query(url_args, order=-i if i == order_by else i)})'
+        for i, v in enumerate(gift_stats_line_list, start=1))
+    md += get_md_table_line([':--:'] * len(gift_stats_line_list))
+    with db.session() as session:
+        lines = session.query(*(v.obj for v in gift_stats_line_list)) \
+            .filter_by(type=gtype, rid=room_id).order_by(get_order_obj(order_by))
+        if aggregate:
+            lines = lines.group_by(GiftStatsTable.uid, GiftStatsTable.gid)
+        else:
+            lines = lines.group_by(GiftStatsTable.uid, GiftStatsTable.gid, GiftStatsTable.time)
+    md += ''.join(get_md_from_line(line) for line in lines)
+    return md
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
