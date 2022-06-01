e19a2de45c6305a5a7dbc5154b84b70167dd43d3
翁喆 <zhe_weng@163.com>
2022-06-02 02:16:14 +0800

    change working folder in run_server.py

--- run_server.py
+++ run_server.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.split(os.path.realpath(__file__))[0])
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 from web_app import app as application
 
