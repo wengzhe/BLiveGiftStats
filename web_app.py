0d3111e2b7759d897aebad3be106fdf9be3a1656
翁喆 <zhe_weng@163.com>
2022-06-12 21:28:29 +0800

    bugfix for typo dry_run

--- web_app.py
+++ web_app.py
@@ -44,7 +44,7 @@ def sync_live_data(room_id: int):
     min_time, max_time = request.args.get('min', None), request.args.get('max', None)
     min_time = Utils.time_param_to_unix(min_time) if min_time else None
     max_time = Utils.time_param_to_unix(max_time) if max_time else None
-    dry_run = request.args.get('render', '').lower() != 'false'
+    dry_run = request.args.get('dry_run', '').lower() != 'false'
     text = db_sync(config.sync_database_url(), room_id, min_time, max_time, dry_run)
     if request.args.get('render', '').lower() != 'false':
         text = Utils.render_markdown(text, f"Sync {room_id}\n")
